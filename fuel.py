48c386bef5d1627ebabcc2315536fae6ad68390d
Robert Ratliff <robert.ratliff@code42.com>
2019-12-03 20:54:57 -0600

    Day 1 part 2, more accurate fuel calculation.

--- fuel.py
+++ fuel.py
@@ -2,15 +2,26 @@ import math
 import unittest
 
 def getFuel(mass):
+    if _getFuelNaive(mass) <= 0:
+        return 0
+    else:
+        return _getFuelNaive(mass) + getFuel(_getFuelNaive(mass))
+
+def _getFuelNaive(mass):
     return math.floor(mass/3) - 2
 
 class TestFuelCalculation(unittest.TestCase):
 
-    def test_examples(self):
-        self.assertEqual(getFuel(12), 2)
+    def test_getFuelNaive(self):
+        self.assertEqual(_getFuelNaive(12), 2)
+        self.assertEqual(_getFuelNaive(14), 2)
+        self.assertEqual(_getFuelNaive(1969), 654)
+        self.assertEqual(_getFuelNaive(100756), 33583)
+
+    def test_getFuel(self):
         self.assertEqual(getFuel(14), 2)
-        self.assertEqual(getFuel(1969), 654)
-        self.assertEqual(getFuel(100756), 33583)
+        self.assertEqual(getFuel(1969), 966)
+        self.assertEqual(getFuel(100756), 50346)
 
 if __name__ == '__main__':
     unittest.main()
